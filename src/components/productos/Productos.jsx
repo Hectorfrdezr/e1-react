091785039f7a4bc8440b7874ba74866b97aad873
hector fernandez <hectorfrdezr@gmail.com>
2024-06-30 21:18:31 -0400

    e1-react

---
+++ src/components/productos/Productos.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from "styled-components"
+
+function Productos() {
+  return (
+        <Product>
+            
+        </Product>
+  )
+}
+
+const Product = styled.div`
+
+`
+export default Productos
