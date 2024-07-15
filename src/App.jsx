0e38a73d915bc071744f29016e5c91ce81163274
hector fernandez <hectorfrdezr@gmail.com>
2024-07-14 21:08:41 -0400

    e1-react

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Paginas/Home';
-import Layaot from './components/Paginas/Layaot';
 function App() {
   
   return (
    <>
-   
+   <Navbar/>
    <Home/>
    </>
   )
